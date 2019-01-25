b9fca86eeb1542fffc03997422bc8e18f5ece1ca
Denis Shirokikh <denisshirokikh@gmail.com>
2019-01-25 00:03:56 -0800

    added model view controller for test modules

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+
   resources :projects
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'projects#index'
 
+  resources :projects do
+    resources :testmodule, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  end
+  #resources :testmodule, only: [:show, :edit, :update, :destroy]}
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_23_053807) do
+ActiveRecord::Schema.define(version: 2019_01_25_052609) do
 
   create_table "projects", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "testmodules", force: :cascade do |t|
+    t.string "title"
+    t.string "content"
+    t.integer "project_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
