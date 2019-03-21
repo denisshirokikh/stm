ea40d7b823deb0afa7630789892c03585f8ed5c1
Denis Shirokikh <denisshirokikh@gmail.com>
2019-03-20 22:25:13 -0700

    validation issue fixed in testcase model copy button fixed

--- app/models/testcase.rb
+++ app/models/testcase.rb
@@ -1,6 +1,9 @@
 class Testcase < ApplicationRecord
-  belongs_to :testmodule
-  #validates :testmodule_id, presence: true
+  belongs_to :testmodule, :inverse_of => :testcases
+  validates_presence_of :testmodule
+  amoeba do
+    enable
+  end
 
 
 end
