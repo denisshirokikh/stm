d151dcbbfe14da2d0866feb1af2d98bf90767448
Denis Shirokikh <denisshirokikh@gmail.com>
2019-05-17 09:54:53 -0700

    dropdown added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,9 @@
 // about supported directives.
 //
 //= require rails-ujs
-= require activestorage
+
 //= require turbolinks
+//= require jquery3
+//= require bootstrap-sprockets
+//= require bootstrap/dropdown
 //= require_tree .
